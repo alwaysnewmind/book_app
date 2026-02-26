1f0e05c1835aad0e2186cf8f24e385f0d02b419d
alwaysnewmind <mukeshpatel200871@gmail.com>
2026-02-26 18:05:02 +0530

    Fix broken auth model imports after merge

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../models/user_model.dart';
+import '../models/user_model.dart';
 import '../services/auth_service.dart';
 
 class AuthProvider extends ChangeNotifier {
--- lib/services/auth_state.dart
+++ lib/services/auth_state.dart
@@ -1,4 +1,4 @@
-import '../../models/user_model.dart';
+import '../models/user_model.dart';
 
 class AuthState {
   final AppUser? user;
